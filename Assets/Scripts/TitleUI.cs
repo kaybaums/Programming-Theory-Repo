fda825948d912add8a5e508e4622d506f3b5ba1c
kaybaums <kirsten.bauman95@gmail.com>
2024-05-31 14:04:31 -0700

    finished title screen
    - created ui script
    - attached button actions
    - can load scenes and quit game

---
+++ Assets/Scripts/TitleUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TitleUI : MonoBehaviour
+{
+    public void LoadDesert()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadForest()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
